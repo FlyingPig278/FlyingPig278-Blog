1d26b858b814cabba8cd2da2ebd1c64ced253897
FlyingPig278 <88129602+FlyingPig278@users.noreply.github.com>
2024-04-05 15:25:21 +0000

    Deploying to gh-pages from @ FlyingPig278/FlyingPig278-Blog@082dfd8d00f281a5dc4c8c528a200b0f42c80a63 🚀

--- assets/pageview-CqX7F01v.js
+++
@@ -1,1 +0,0 @@
-import{L as o,M as n}from"./app-Brb64NgT.js";const s=async()=>{const t=o();try{const{pageviewCount:e}=await n(()=>import("./app-Brb64NgT.js").then(r=>r.N),[]);return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{s as updatePageview};
