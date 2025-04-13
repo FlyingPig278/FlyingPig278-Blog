c9759dc1f9602b69c883f6c4037b2e38cecb21bb
FlyingPig278 <hello_ysy@163.com>
2025-04-13 10:50:35 +0800

    优化样式和主题配置，更新图标引用格式

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,14 +3,14 @@ import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
 export default hopeTheme({
-  hostname: "https://flyingpig.top",
+  hostname: "https://flyingpig278.github.io/FlyingPig278-Blog",
   author: {
     name: "FlyingPig278",
-    url: "https://flyingpig.top",
+    url: "https://flyingpig278.github.io/FlyingPig278-Blog/",
     email: "hello_ysy@163.com"
   },
 
-  iconAssets: "fontawesome-with-brands",
+  // iconAssets: "fontawesome-with-brands",
 
   logo: "/logo.ico",
 
@@ -80,32 +80,11 @@ export default hopeTheme({
   metaLocales: {
     editLink: "编辑此页",
   },
-
   // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
   // hotReload: true,
-
-  // 在这里配置主题提供的插件
-  plugins: {
-    blog: true,
-
-    searchPro: true,
-    
-    // 启用之前需安装 @waline/client
-    // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
-    comment: {
-      provider: "Waline",
-      serverURL: "https://comment.flyingpig.top/",
-    },
-
-    components: {
-      components: ["Badge", "VPCard"],
-    },
-
-    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
-    mdEnhance: {
+  markdown:{
       align: true,
       attrs: true,
-      codetabs: true,
       component: true,
       demo: true,
       figure: true,
@@ -130,45 +109,97 @@ export default hopeTheme({
       sup: true,
       tabs: true,
       vPre: true,
-
-      // 在启用之前安装 chart.js
-      // chart: true,
-
-      // insert component easily
-
-      // 在启用之前安装 echarts
-      // echarts: true,
-
-      // 在启用之前安装 flowchart.ts
-      // flowchart: true,
-
-      // gfm requires mathjax-full to provide tex support
-      // gfm: true,
-
-      // 在启用之前安装 katex
-      // katex: true,
-
-      // 在启用之前安装 mathjax-full
-      // mathjax: true,
-
-      // 在启用之前安装 mermaid
-      // mermaid: true,
-
-      // playground: {
-      //   presets: ["ts", "vue"],
-      // },
-
-      // 在启用之前安装 reveal.js
-      revealJs: {
-        plugins: ["highlight", "math", "search", "notes", "zoom"],
-      },
-
-      // 在启用之前安装 @vue/repl
-      // vuePlayground: true,
-
-      // install sandpack-vue3 before enabling it
-      // sandpack: true,
+  },
+  // 在这里配置主题提供的插件
+  plugins: {
+    icon:{
+      assets:"fontawesome-with-brands"
     },
+
+    blog: true,
+
+    slimsearch: true,
+    
+    // 启用之前需安装 @waline/client
+    // 警告: 这是一个仅供演示的测试服务，在生产环境中请自行部署并使用自己的服务！
+    // comment: {
+    //   provider: "Waline",
+    //   serverURL: "https://comment.flyingpig.top/",
+    // },
+
+    components: {
+      components: ["Badge", "VPCard"],
+    },
+
+    // 此处开启了很多功能用于演示，你应仅保留用到的功能。
+    
+    // mdEnhance: {
+    //   align: true,
+    //   attrs: true,
+    //   codetabs: true,
+    //   component: true,
+    //   demo: true,
+    //   figure: true,
+    //   imgLazyload: true,
+    //   imgSize: true,
+    //   include: true,
+    //   mark: true,
+    //   stylize: [
+    //     {
+    //       matcher: "Recommended",
+    //       replacer: ({ tag }) => {
+    //         if (tag === "em")
+    //           return {
+    //             tag: "Badge",
+    //             attrs: { type: "tip" },
+    //             content: "Recommended",
+    //           };
+    //       },
+    //     },
+    //   ],
+    //   sub: true,
+    //   sup: true,
+    //   tabs: true,
+    //   vPre: true,
+
+    //   // 在启用之前安装 chart.js
+    //   // chart: true,
+
+    //   // insert component easily
+
+    //   // 在启用之前安装 echarts
+    //   // echarts: true,
+
+    //   // 在启用之前安装 flowchart.ts
+    //   // flowchart: true,
+
+    //   // gfm requires mathjax-full to provide tex support
+    //   // gfm: true,
+
+    //   // 在启用之前安装 katex
+    //   // katex: true,
+
+    //   // 在启用之前安装 mathjax-full
+    //   // mathjax: true,
+
+    //   // 在启用之前安装 mermaid
+    //   // mermaid: true,
+
+    //   // playground: {
+    //   //   presets: ["ts", "vue"],
+    //   // },
+
+    //   // 在启用之前安装 reveal.js
+    //   revealJs: {
+    //     plugins: ["highlight", "math", "search", "notes", "zoom"],
+    //   },
+
+    //   // 在启用之前安装 @vue/repl
+    //   // vuePlayground: true,
+
+    //   // install sandpack-vue3 before enabling it
+    //   // sandpack: true,
+    // },
   },
 }, {
   //主题行为选项
--- src/collection/README.md
+++ src/collection/README.md
@@ -3,12 +3,12 @@ icon: star
 article: false
 ---  
 # 收藏夹
-## <HopeIcon icon="book"/>博客  
+## ::icon book::博客  
 [点击前往](blog.md)  
 
-## <HopeIcon icon="screwdriver-wrench"/>工具
-#### <HopeIcon icon="link"/>网站: [点击前往](tool/website.md)    
-#### <HopeIcon icon="computer"/>软件: [点击前往](tool/software.md)
+## ::icon screwdriver-wrench::工具
+#### ::icon link::网站: [点击前往](tool/website.md)    
+#### ::icon computer::软件: [点击前往](tool/software.md)
 
-## <HopeIcon icon="gamepad"/>游戏
+## ::icon gamepad::游戏
 [点击前往](game.md)
